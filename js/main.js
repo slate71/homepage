e495a114bd67e3e50cdfd262e32d08fb72989cb6
Lukas Andersen <luke@slate71.com>
2016-10-26 08:33:04 -0700

    Refactor js and and forms index.html

--- js/main.js
+++ js/main.js
@@ -1,20 +1,28 @@
-window.addEventListener('pageshow', function() {
-  var bg = document.querySelector('.bgImage');
-  var nav = document.querySelector('nav');
-  var quote = document.querySelector('article');
-  var aside = document.querySelector('aside');
+(function() {
+  const $ = document.querySelector.bind(document);
+  const bg = $('.bgImage');
+  const nav = $('nav');
+  const quote = $('article');
+  const aside = $('aside');
 
-  // Don't animate until image is loaded... optimize image
-  Velocity(quote,
-           {opacity: 1},
-           {delay: 500, duration: 1000, display: 'flex'});
-  Velocity(bg,
-           {opacity: .6 },
-           {delay: 500, duration: 1000, display: 'block'})
-    .then(() => Velocity(nav,
-                         'slideDown',
-                         {delay: 500, duration: 500, display: 'flex'}));
-  Velocity(aside,
-           {opacity: 1},
-           {delay: 500, duration: 1000, display: 'flex'});
-});
+  function handlePageshow(event) {
+    window.removeEventListener('pageshow', handlePageshow);
+
+    Velocity(quote,
+             {opacity: 1},
+             {delay: 500, duration: 1000, display: 'flex'});
+
+    Velocity(bg,
+             {opacity: .6 },
+             {delay: 500, duration: 1000, display: 'block'})
+      .then(() => Velocity(nav,
+                           'slideDown',
+                           {delay: 500, duration: 500, display: 'flex'}));
+
+    Velocity(aside,
+             {opacity: 1},
+             {delay: 500, duration: 1000, display: 'flex'});
+  }
+
+  window.addEventListener('pageshow', handlePageshow);
+})();
